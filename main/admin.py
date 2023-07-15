4d678a0ea75e2903e2f38bcd2d75bafde19ef0dd
unknown <suraj.acharya62@gmail.com>
2023-07-15 13:14:24 +0200

    initial commit repo initialization

---
+++ main/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from . models import (
+    UserProfile,
+    ContactProfile,
+    Testimonial,
+    Media,
+    Portfolio,
+    Blog,
+    Certificate,
+    Skill,
+    Experience
+    )
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user')
+
+@admin.register(ContactProfile)
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('id', 'timestamp', 'name',)
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('id','name','is_active')
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('id','name','is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id','name','is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id','name','score')
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('id','start_date','end_date','company_name','job_title','description')
